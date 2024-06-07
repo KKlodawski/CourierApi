00d0552653e61bde4b103abc28f90a6847e0d4df
KKlodawski <s24777@pjwstk.edu.pl>
2024-06-07 14:33:31 +0200

    - Added changePassword route - Minor changed - Added ability to draw on pictures - Added local SQLite database

--- api/routes/couriers.js
+++ api/routes/couriers.js
@@ -1,3 +1,37 @@
 var express = require('express');
+const {verifyToken, Roles} = require("../verification");
 var router = express.Router();
 module.exports = router;
+var { courier } = require('../models');
+router.get("/", verifyToken([Roles.MANAGER]) ,async(req,res) => {
+    try {
+        const couriers = await courier.findAll();
+        let dt = [];
+        couriers.forEach((cr) => {
+            const data = {
+                IdCourier: cr.IdCourier,
+                Name: cr.Name,
+                Surname: cr.Surname,
+                EmploymentDate: cr.EmploymentDate,
+                CourierStatus: cr.CourierStatus,
+                IsActive: cr.IsActive
+            }
+            dt.push(data)
+        })
+        for (let i = 0; i < 10; i++) {
+            const data = {
+                IdCourier: `FabicatedData${i}`,
+                Name: `FabicatedData${i}`,
+                Surname: `FabicatedData${i}`,
+                EmploymentDate: `FabicatedData${i}`,
+                CourierStatus: `FabicatedData${i}`,
+                IsActive: false
+            }
+            dt.push(data)
+        }
+        res.status(200).json(dt);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+
+})
